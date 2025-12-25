71aa2f9806271ee98e2a3cc14e9f2ab06bf3b693
Sauhardya <sauhardyachakraborty2643@gmail.com>
2025-12-25 13:08:23 +0530

    Updated server.ts and package.json

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,31 @@
-import app from './app.js'
-import { env } from './config/env.js'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import chatRoutes from "./routes/chatRoutes.js";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import { env } from "./config/env.js";
+
+const app = express();
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use("/api/chat", chatRoutes);
+
+app.use(errorMiddleware);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 app.listen(env.port, () => {
-  console.log(`Server running on port ${env.port}`)
-})
+  console.log(`Server is listening on PORT: ${env.port}`);
+});
